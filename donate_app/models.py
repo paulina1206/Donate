eb42a37aa61ee2e896c7699f02482300427f8845
Paulina <paulina12061993@gmail.com>
2020-07-16 23:17:13 +0200

    LandingPage

--- donate_app/models.py
+++ donate_app/models.py
@@ -48,3 +48,4 @@ class Donation(models.Model):
     pick_up_time = models.TimeField()
     pick_up_comment = models.TextField(null=True, default=None)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+
--- donate_app/views.py
+++ donate_app/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+from donate_app.models import Institution, Donation
 
 
 # Create your views here.
@@ -7,7 +9,10 @@ from django.views import View
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        number_of_supported_institution = Institution.objects.all().count() #czy to ma być liczba instytucji, czy ilość wszystkich wsparć
+        sum_of_donated_quantity = Donation.objects.aggregate(Sum('quantity')) #https://docs.djangoproject.com/en/3.0/topics/db/aggregation/
+        return render(request, 'index.html', {"no_institution": number_of_supported_institution,
+                                              "sum_quantity": sum_of_donated_quantity, })
 
 
 class AddDonation(View):
