726b330a70ce925b6bdfacfcf540af70ae8d6175
Paulina <paulina12061993@gmail.com>
2020-07-17 16:12:39 +0200

    LandingPage_pagination

--- donate_app/views.py
+++ donate_app/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
@@ -11,8 +12,23 @@ class LandingPage(View):
     def get(self, request):
         number_of_supported_institution = Institution.objects.all().count() #czy to ma być liczba instytucji, czy ilość wszystkich wsparć
         sum_of_donated_quantity = Donation.objects.aggregate(Sum('quantity')) #https://docs.djangoproject.com/en/3.0/topics/db/aggregation/
+        fundations = Institution.objects.filter(type=1)
+        paginator_fundations = Paginator(fundations, 1)
+        page = request.GET.get('page')
+        p_fundations = paginator_fundations.get_page(page)
+        organizations = Institution.objects.filter(type=2)
+        paginator_organizations = Paginator(organizations, 1)
+        page = request.GET.get('page')
+        p_organizations = paginator_organizations.get_page(page)
+        collections = Institution.objects.filter(type=3)
+        paginator_collections = Paginator(collections, 1)
+        page = request.GET.get('page')
+        p_collections = paginator_collections.get_page(page) #https://docs.djangoproject.com/en/2.2/topics/pagination/
         return render(request, 'index.html', {"no_institution": number_of_supported_institution,
-                                              "sum_quantity": sum_of_donated_quantity, })
+                                              "sum_quantity": sum_of_donated_quantity,
+                                              "fundations": p_fundations,
+                                              "organizations": p_organizations,
+                                              "collections": p_collections})
 
 
 class AddDonation(View):
